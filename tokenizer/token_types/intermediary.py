c99e6935d58b957d9075047a2245ac2a43fa8951
Mihnea Clejan <contact@mihneaclejan.com>
2025-09-06 23:42:52 +0100

    Add more token types

--- tokenizer/token_types/intermediary.py
+++ tokenizer/token_types/intermediary.py
@@ -22,3 +22,33 @@ class Add(Token):
 @Tokenizer.add_type
 class Identifier(Token):
     re = re.compile("[a-zA-Z]\w*")
+
+
+@Tokenizer.add_type
+class Recall(Token):
+    re = re.compile("_")
+
+
+@Tokenizer.add_type
+class Placeholder(Token):
+    re = re.compile("\\$")
+
+
+@Tokenizer.add_type
+class Comma(Token):
+    re = re.compile(",")
+
+
+@Tokenizer.add_type
+class Lparen(Token):
+    re = re.compile("\\(")
+
+
+@Tokenizer.add_type
+class Rparen(Token):
+    re = re.compile("\\)")
+
+
+@Tokenizer.add_type
+class Pipe(Token):
+    re = re.compile("->")
